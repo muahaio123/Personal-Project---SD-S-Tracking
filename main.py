dc2578332f41e6448abea621568604efa9467449
Long Le <longle9a@gmail.com>
2024-01-26 16:25:47 -0600

    initialize sitename, employee list  main functions

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+
+project_path = 'G:\\SD&S Tracking'
+employee_list: list[str] = []
+
+# create new site folder and copy the am1 and 2 files into it
+def initialize_sitename(s: str):
+    am_source_files_path = os.getcwd() + "\\am_source_files"
+
+    # create new folder for that site
+    sitename_path = project_path + '\\' + s
+    if not os.path.exists(sitename_path):
+        os.makedirs(sitename_path)
+    
+    # copy the am1 am2 source files to that folder
+    # list all items inside AM_source_files
+    for filename in os.listdir(am_source_files_path):
+        source_file = am_source_files_path + "\\" + filename
+        destination_file = sitename_path + "\\" + filename
+
+        # Check if source is a file and not exist in destination folder
+        if os.path.isfile(source_file) and not os.path.exists(destination_file):
+            shutil.copy(source_file, destination_file)
+
+def initialize_employee_list():
+    try:
+        with open("employees_list.txt", 'r') as file:
+            content = file.read()
+            employee_list = content.splitlines()
+            print("current employee list: " + ', '.join(employee_list))
+    except FileNotFoundError:
+        print(f"The file was not found!")
+
+def main():
+    initialize_employee_list()
+
+    if not os.path.exists(project_path):
+        os.makedirs(project_path)
+
+    print(os.listdir(project_path))
+    sitename_list: list[str] = os.listdir(project_path)
+
+    sitename_new: str = input("Enter site name to create new: ")
+    initialize_sitename(sitename_new)
+
+if __name__ == "__main__":
+    main()
